b01797a2235a496daac18d395127459249d5306f
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2018-09-12 00:09:25 -0700

    debug comments

--- app/src/main/res/raw/private_script.js
+++ app/src/main/res/raw/private_script.js
@@ -1,11 +1,16 @@
 (() => {
+  document.body.innerHTML += '<p>script</p>';
   const urls = {
     private_mode: 'https://support.mozilla.org/kb/private-mode-firefox-reality'
   };
   window.addEventListener('load', () => {
+    document.body.innerHTML += '<p>load</p>';
     const aboutInfoEl = document.querySelector('.about-info');
     if (aboutInfoEl) {
+      document.body.innerHTML += '<p>about info found</p>';
       aboutInfoEl.href = urls.private_mode;
+    } else {
+      document.body.innerHTML += '<p>about info not found</p>';
     }
   });
 })();
